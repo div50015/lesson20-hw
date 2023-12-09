1ee5e5a71f1cd37609a40abd4c360c347b5d89a0
Igor Degtyarenko <div50015@mail.ru>
2023-12-09 21:59:07 +0300

    Login & add lantop smartphone book OK.

--- tests/test_demoshop.py
+++ tests/test_demoshop.py
@@ -15,19 +15,53 @@ WEB_URL = "https://demowebshop.tricentis.com/"
 API_URL = "https://demowebshop.tricentis.com/"
 
 
-def test_login_though_api():
+def test_login_through_api():
     result = requests.post(
         url=API_URL + "/login",
         data={"Email": LOGIN, "Password": PASSWORD, "RememberMe": False},
         allow_redirects=False
     )
     assert result.status_code == 302
+
+    global cookie
     cookie = result.cookies.get("NOPCOMMERCE.AUTH")
 
+    browser.open(WEB_URL)
+    browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": cookie})
+    browser.open(WEB_URL)
+
+    browser.element('.account').should(have.text('example1200@example.com'))
+
+
+
+
+def test_add_laptop_to_cart():
     result = requests.post(url=API_URL + '/addproducttocart/details/31/1', data={'addtocart_31.EnteredQuantity': 1}, cookies={"NOPCOMMERCE.AUTH": cookie})
     assert result.status_code == 200
+
+    browser.open(WEB_URL)
+    browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": cookie})
+    browser.open(WEB_URL)
+    browser.element('#topcartlink > a > span.cart-label').click()
+
+    browser.element('.cart').should(have.text('14.1-inch Laptop'))
+
+
+
+def test_add_smartphone_to_cart():
     result = requests.post(url=API_URL + '/addproducttocart/details/43/1', data={'addtocart_43.EnteredQuantity': 1}, cookies={"NOPCOMMERCE.AUTH": cookie})
     assert result.status_code == 200
+
+    browser.open(WEB_URL)
+    browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": cookie})
+    browser.open(WEB_URL)
+
+    browser.element('#topcartlink > a > span.cart-label').click()
+
+    browser.element('.cart').should(have.text('Smartphone'))
+
+
+def test_add_book_to_cart():
     result = requests.post(url=API_URL + '/addproducttocart/details/22/1', data={'addtocart_22.EnteredQuantity': 1}, cookies={"NOPCOMMERCE.AUTH": cookie})
     assert result.status_code == 200
 
@@ -35,9 +69,9 @@ def test_login_though_api():
     browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": cookie})
     browser.open(WEB_URL)
 
-
-    browser.element(".account").should(have.text(LOGIN))
-
     browser.element('#topcartlink > a > span.cart-label').click()
-    sleep(5)
+
+    browser.element('.cart').should(have.text('Health Book'))
+
+
 
