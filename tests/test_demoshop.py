93f6d995a2c09ca9a946eba5622633f41b57508d
Igor Degtyarenko <div50015@mail.ru>
2023-12-10 16:53:53 +0300

    Add allure report

--- tests/test_demoshop.py
+++ tests/test_demoshop.py
@@ -4,74 +4,126 @@ from allure_commons.types import AttachmentType
 from selene import browser
 from selene.support.conditions import have
 import allure
+import json
+import logging
 from time import sleep
 
 
-# LOGIN = "test@qa.guru.com"
-# PASSWORD = "123456"
-LOGIN = "example1200@example.com"
+LOGIN = "test@qa.guru.com"
 PASSWORD = "123456"
+# LOGIN = "example1200@example.com"
+# PASSWORD = "123456"
 WEB_URL = "https://demowebshop.tricentis.com/"
 API_URL = "https://demowebshop.tricentis.com/"
 
 
 def test_login_through_api():
-    result = requests.post(
-        url=API_URL + "/login",
-        data={"Email": LOGIN, "Password": PASSWORD, "RememberMe": False},
-        allow_redirects=False
-    )
-    assert result.status_code == 302
+    with step("Login with API"):
+        result = requests.post(
+            url=API_URL + "/login",
+            data={"Email": LOGIN, "Password": PASSWORD, "RememberMe": False},
+            allow_redirects=False
+        )
+        assert result.status_code == 302
+        allure.attach(body=result.text, name="Response", attachment_type=AttachmentType.TEXT, extension="txt")
+        allure.attach(body=str(result.cookies), name="Cookies", attachment_type=AttachmentType.TEXT, extension="txt")
+        logging.info(result.request.url)
+        logging.info(result.status_code)
+        logging.info(result.text)
+
 
     global cookie
-    cookie = result.cookies.get("NOPCOMMERCE.AUTH")
+    with step("Get cookie from API"):
+        cookie = result.cookies.get("NOPCOMMERCE.AUTH")
 
-    browser.open(WEB_URL)
-    browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": cookie})
-    browser.open(WEB_URL)
+    with step("Set cookie from API and open web url"):
+        browser.open(WEB_URL)
+        browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": cookie})
+        browser.open(WEB_URL)
 
-    browser.element('.account').should(have.text('example1200@example.com'))
+    with step("Verify successful authorization"):
+        browser.element('.account').should(have.text(f'{LOGIN}'))
 
 
 
 
 def test_add_laptop_to_cart():
-    result = requests.post(url=API_URL + '/addproducttocart/details/31/1', data={'addtocart_31.EnteredQuantity': 1}, cookies={"NOPCOMMERCE.AUTH": cookie})
-    assert result.status_code == 200
+    with step("Adding to cart a lapton"):
+        result = requests.post(
+            url=API_URL + '/addproducttocart/details/31/1',
+            data={'addtocart_31.EnteredQuantity': 1},
+            cookies={"NOPCOMMERCE.AUTH": cookie}
+        )
+        assert result.status_code == 200
+        # allure.attach(body=result.text, name="Response", attachment_type=AttachmentType.TEXT, extension="txt")
+        allure.attach(body=json.dumps(result.json(), indent=4, ensure_ascii=True), name="Response", attachment_type=AttachmentType.JSON, extension="json")
+        allure.attach(body=str(result.cookies), name="Cookies", attachment_type=AttachmentType.TEXT, extension="txt")
+        logging.info(result.request.url)
+        logging.info(result.status_code)
+        logging.info(result.text)
 
-    browser.open(WEB_URL)
-    browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": cookie})
-    browser.open(WEB_URL)
-    browser.element('#topcartlink > a > span.cart-label').click()
 
-    browser.element('.cart').should(have.text('14.1-inch Laptop'))
+    with step("Set cookie from API and open web url"):
+        browser.open(WEB_URL)
+        browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": cookie})
+        browser.open(WEB_URL)
+        browser.element('#topcartlink > a > span.cart-label').click()
+
+    with step('Checking cart for added latop'):
+            browser.element('.cart').should(have.text('14.1-inch Laptop'))
 
 
 
 def test_add_smartphone_to_cart():
-    result = requests.post(url=API_URL + '/addproducttocart/details/43/1', data={'addtocart_43.EnteredQuantity': 1}, cookies={"NOPCOMMERCE.AUTH": cookie})
-    assert result.status_code == 200
+    with step("Adding to cart a smartphone"):
+        result = requests.post(
+            url=API_URL + '/addproducttocart/details/43/1',
+            data={'addtocart_43.EnteredQuantity': 1},
+            cookies={"NOPCOMMERCE.AUTH": cookie}
+        )
+        assert result.status_code == 200
+        allure.attach(body=json.dumps(result.json(), indent=4, ensure_ascii=True), name="Response", attachment_type=AttachmentType.JSON, extension="json")
+        # allure.attach(body=result.text, name="Response", attachment_type=AttachmentType.TEXT, extension="txt")
+        allure.attach(body=str(result.cookies), name="Cookies", attachment_type=AttachmentType.TEXT, extension="txt")
+        logging.info(result.request.url)
+        logging.info(result.status_code)
+        logging.info(result.text)
 
-    browser.open(WEB_URL)
-    browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": cookie})
-    browser.open(WEB_URL)
 
-    browser.element('#topcartlink > a > span.cart-label').click()
+    with step("Set cookie from API and open web url"):
+        browser.open(WEB_URL)
+        browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": cookie})
+        browser.open(WEB_URL)
+        browser.element('#topcartlink > a > span.cart-label').click()
 
-    browser.element('.cart').should(have.text('Smartphone'))
+    with step('Checking cart for added smartphone'):
+        browser.element('.cart').should(have.text('Smartphone'))
 
 
 def test_add_book_to_cart():
-    result = requests.post(url=API_URL + '/addproducttocart/details/22/1', data={'addtocart_22.EnteredQuantity': 1}, cookies={"NOPCOMMERCE.AUTH": cookie})
-    assert result.status_code == 200
+    with step("Adding to cart a book"):
+        result = requests.post(
+            url=API_URL + '/addproducttocart/details/22/1',
+            data={'addtocart_22.EnteredQuantity': 1},
+            cookies={"NOPCOMMERCE.AUTH": cookie}
+        )
+        assert result.status_code == 200
+        # allure.attach(body=result.text, name="Response", attachment_type=AttachmentType.TEXT, extension="txt")
+        allure.attach(body=json.dumps(result.json(), indent=4, ensure_ascii=True), name="Response", attachment_type=AttachmentType.JSON, extension="json")
+        allure.attach(body=str(result.cookies), name="Cookies", attachment_type=AttachmentType.TEXT, extension="txt")
+        logging.info(result.request.url)
+        logging.info(result.status_code)
+        logging.info(result.text)
 
-    browser.open(WEB_URL)
-    browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": cookie})
-    browser.open(WEB_URL)
 
-    browser.element('#topcartlink > a > span.cart-label').click()
+    with step("Set cookie from API and open web url"):
+        browser.open(WEB_URL)
+        browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": cookie})
+        browser.open(WEB_URL)
+        browser.element('#topcartlink > a > span.cart-label').click()
 
-    browser.element('.cart').should(have.text('Health Book'))
+    with step('Checking cart for added book'):
+        browser.element('.cart').should(have.text('Health Book'))
 
 
 
